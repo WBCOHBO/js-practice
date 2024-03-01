48f444e3aee5b848e9167c5a682ee5e1b8d4216e
WBCOHBO <andy405641@gmail.com>
2024-03-01 11:41:05 +0800

    新增 JS 功能 - 點擊顏色方塊後，執行 innerHTML 顯示對應的文字與顏色

--- dom_red-yellow-green/script.js
+++ dom_red-yellow-green/script.js
@@ -15,3 +15,15 @@ function changeTitle(){
 // click button
 let changeButton = document.getElementById("changeButton");
 changeButton.addEventListener("click", changeTitle);
+
+const squares = document.querySelectorAll('.colorSquare');
+const output = document.getElementById('output');
+
+// function square 執行 square的 onclick方法
+// 裡面執行 getId 與 innerHTML
+squares.forEach(square => {
+    square.onclick = () => {
+        _id = square.getAttribute('id');
+        output.innerHTML = `<div class = ".colorSquare" id = "${_id}">${_id}</div>`;
+    }
+})
